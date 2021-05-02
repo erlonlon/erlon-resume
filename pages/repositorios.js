e8020ab2ea72baf8625eecd15c4bcd4a1f44e1c0
Erlon Carlos de Oliveira Gomes <erlonlon@gmail.com>
2021-05-02 10:02:20 -0300

    Inicial do projeto curriculo

---
+++ pages/repositorios.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import getUser from '../utils/getUser'
+
+const Index = ({ repos, user }) => {
+
+  return (
+
+    <>
+      <div className="md:container md:mx-auto px-4">
+        <h1 className='text-5xl' >Erlon Carlos</h1>
+
+        <h2 className='font-bold text-5x1'>Meus Repositórios</h2>
+        <p>Git stats: public repos: {user.public_repos} / public_gists: {user.public_gists} / followers: {user.followers}</p>
+        {repos.map(repo => {
+          return (
+            <div key={repo.id} className='rounded bg-gray-200 mx-8 my-4 p-4 hover:shadow-md'>
+              <h3 className='font-bold'>{repo.full_name}</h3>
+              <p>{repo.language} / Stars: {repo.stargazers_count}</p>
+            </div>
+          )
+        })}
+      </div>
+    </>
+  )
+}
+export async function getServerSideProps(context) {
+
+  const { repos, user } = await getUser('erlonlon')
+
+  return {
+    props: {
+
+      repos,
+      user
+    }
+  }
+}
+
+export default Index
---
+++ utils/getUser.js
@@ -0,0 +1,33 @@
+const getUser = async (userName) => {
+
+  const resUser = await fetch('https://api.github.com/users/' + userName)
+  const user = await resUser.json()
+
+  const resRepos = await fetch(`https://api.github.com/users/${userName}/repos?sort=updated`)
+  const originalRepos = await resRepos.json()
+
+  const dontShowRepos = [
+    'erlonlon/pousadaparaiso',
+    'erlonlon/blog',
+    'erlonlon/erro',
+    'erlonlon/t',
+    'erlonlon/teste01',
+    'erlonlon/jocelio',
+    'erlonlon/cv'
+  ]
+  const dontShowFilter = repo => dontShowRepos.indexOf(repo.full_name) === -1
+  const extractRepos = repo => ({
+    id: repo.id,
+    full_name: repo.full_name,
+    language: repo.language,
+    stargazers_count: repo.stargazers_count
+  })
+  const repos = originalRepos.filter(dontShowFilter).map(extractRepos)
+
+  return {
+    repos, user
+  }
+
+
+}
+export default getUser
