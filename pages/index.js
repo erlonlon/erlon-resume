56729a769aca72c96cf15dbbc54d76a280fd9bf4
Erlon Carlos de Oliveira Gomes <erlonlon@gmail.com>
2021-05-05 22:10:00 -0300

    bloco rede social

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import getUser from '../utils/getUser'
+import { FaLinkedin, FaGithub, FaYoutube } from 'react-icons/fa'
+import { FiLink } from 'react-icons/fi'
 
 const Index = ({ repos, user }) => {
 
@@ -14,30 +16,40 @@ const Index = ({ repos, user }) => {
           </div>
           <div className=' pt-24 m-12'>
             <h1 className='font-bold text-7xl uppercase py-2' >Erlon Carlos</h1>
-            <h1 className='text-4xl uppercase py-2'>FullStack Developer</h1>
-            <h3>Contatos</h3>
-            <ul>
-              <li>linkedin</li>
-            </ul>
-            <p>Git stats: public repos: {user.public_repos} / public_gists: {user.public_gists} / followers: {user.followers}</p>
+            <h1 className='text-4xl uppercase py-4 '>FullStack Developer</h1>
+
+            <div className='relative border-2 rounded px-16 pb-4 pt-10 mt-6 border-green-400'>
+              <h3 className='text-white bg-green-400 absolute py-2 px-6 top-0 -mt-7 text-3xl font-bold uppercase '>Contatos</h3>
+              <p>
+                <FaLinkedin className='py-2 text-6xl inline-block mr-6' />
+                <FaGithub className='py-2 text-6xl inline-block mr-6' />
+                <FaYoutube className='py-2 text-6xl inline-block mr-6' />
+                <FiLink className='py-2 text-6xl inline-block mr-6' />
+                <br></br>
+                <span className='inline-block pt-2 font-bold text-2xl '>erlon@dev</span>
+              </p>
+
+              <p className='py-2 font-bold text-2xl uppercase'>Git stats: public repos: {user.public_repos} / public_gists: {user.public_gists} / followers: {user.followers}</p>
+
+            </div>
           </div>
         </div>
 
 
         <div className="grid grid-flow-col md:grid-flow-col leading-none pt-24">
-          <div className='bg-white rounded-lg shadow-lg py-12 px-16 '>
-            <h3 className='text-4xl uppercase text-center'>Competências</h3>
+          <div className='bg-opacity-30 bg-gray-200 rounded-lg shadow-lg py-12 px-16 '>
+            <h3 className='text-4xl uppercase text-center py-12 text-green-400'>Competências</h3>
             <p className='text-3xl'>Fullstack Developer</p>
 
           </div>
         </div>
         <div className="grid grid-flow-col md:grid-flow-col leading-none">
-          <h3 className='text-4xl uppercase text-center py-12 text-green-300'>
+          <h3 className='text-4xl uppercase text-center py-12 text-green-400'>
             Meus Estudos
             </h3>
         </div>
 
-        <div className="py-12 grid grid-flow-col md:grid-flow-col leading-none bg-white rounded-lg shadow-lg">
+        <div className="py-12 grid grid-flow-col md:grid-flow-col leading-none bg-opacity-30 bg-gray-200 rounded-lg shadow-lg">
 
           {[1, 2].map(i => (
             <div className='border-dotted border-l-4 border-light-blue-500 px-16 '>
@@ -50,11 +62,11 @@ const Index = ({ repos, user }) => {
 
         </div>
 
-        <div class="grid grid-flow-col md:grid-flow-col">
+        <div className="grid grid-flow-col md:grid-flow-col">
           <div className=''>
             {repos.map(repo => {
               return (
-                <div key={repo.id} className='rounded bg-gray-200 mx-8 my-4 p-4 hover:shadow-md'>
+                <div key={repo.id} className='rounded bg-opacity-30 bg-gray-200 mx-8 my-4 p-4 hover:shadow-md'>
                   <h3 className='font-bold'>{repo.full_name}</h3>
                   <p>{repo.language} / Stars: {repo.stargazers_count}</p>
                 </div>
